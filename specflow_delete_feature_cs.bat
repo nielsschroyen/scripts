20d8143f6e9a0c6a7bb2f0f4df2c2cb970413161
Niels Schroyen <niels.schroyen@gmail.com>
2018-02-16 14:30:17 +0100

    delete all feature cs files

--- specflow_delete_feature_cs.bat
+++ specflow_delete_feature_cs.bat
@@ -1,6 +1,4 @@
 REM Specflow uses a cache to speed up the bindings. At times the cache is invalid
 REM This scripts deletes the cache
 REM https://stackoverflow.com/questions/17167820/specflow-error-force-regenerate-steps-possible
-
-cd %TEMP%
-del /s /q /f specflow*.cache
+del /s /q /f *.feature.cs
